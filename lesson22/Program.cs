8f063a54f50532ccc58a6bf2f28acfdf3fd90dba
shawnswrld <shawnswrld@gmail.com>
2017-06-08 12:20:26 -0400

    shawn

---
+++ lesson22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lesson22
+{
+    class Program
+    {
+        static void MultiplytTwoNumbers(int x,int y)
+           int x = 2;
+           int y = 2;
+
+          return (x*y);
+            Console.WriteLine();
+        }
+    }
+}
